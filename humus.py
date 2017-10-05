ac8c810c9b1eb86c1452ef9bb06088623fef2780
Raz1990 <raz2712@gmail.com>
2017-10-05 08:30:12 +0300

    removed hard-coded lines, replaced with arguments

--- humus.py
+++ humus.py
@@ -1,21 +1,25 @@
 from pyquery import PyQuery as pq
 import urllib
 
-d = pq(url='https://store.pass.dmm.com/entry/tickets')
+#path = 'https://store.pass.dmm.com/entry/tickets'
+
+d = pq(url=sys.argv[1])
 
 f = open('site', 'a+')
 f.seek(0)
 
 text = f.read()
-print(text)
+#print(text)
 
-found = d('div:contains("Suchmos")').html()
-print(found)
+#search_word = "Suchmos"
+
+found = d('body:contains('+sys.argv[2]+')').text()
+#print(found)
 
 if (found != text):
-    print("WILL NOW WRITE CHANGES INTO site")
+    #print("WILL NOW WRITE CHANGES INTO site")
     f.write(found)
-else:
-    print("NO NEW CHANGES")
+#else:
+    #print("NO NEW CHANGES")
 
 f.close()
