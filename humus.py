ba9a977a48cfe78104ce9079e48d5131f0379403
Raz1990 <raz2712@gmail.com>
2017-10-05 13:43:51 +0300

    added email funcionality

--- humus.py
+++ humus.py
@@ -19,7 +19,36 @@ found = d('body:contains('+sys.argv[2]+')').text()
 print(found)
 
 if (found != text):
-    print("WILL NOW WRITE CHANGES INTO site")
+    #print("WILL NOW WRITE CHANGES INTO site")
+
 	#if found new content in HTML, overwrite the existing text file with a new one containing all of the new informtaion
 	with open('site','w') as f:
     	f.write(found)
+	
+	# Email.
+	
+	gmail_user = 'WebsiteChangeDetector.R@gmail.com'  
+	gmail_password = '123passwor'
+
+	sent_from = gmail_user  
+	to = [sys.argv[3]]  
+	subject = 'New changes found in ' + sys.argv[1]  
+	body = found
+
+	email_text = """\  
+	From: %s  
+	To: %s  
+	Subject: %s
+
+	%s
+	""" % (sent_from, ", ".join(to), subject, body)
+
+	try:  
+		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+		server.ehlo()
+		server.login(gmail_user, gmail_password)
+		server.sendmail(sent_from, to, email_text)
+		server.close()
+
+	except:  
+		print 'Something went wrong...'
