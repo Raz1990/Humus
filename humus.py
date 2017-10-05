d70238dfdb187ecb52e948ae6eda8948e2d15dda
Raz1990 <raz2712@gmail.com>
2017-10-05 14:39:10 +0300

    fixed white spaces in if statement

--- humus.py
+++ humus.py
@@ -1,5 +1,6 @@
 from pyquery import PyQuery as pq
 import urllib
+import smtplib
 
 site = sys.argv[1]
 search_word = sys.argv[2]
@@ -9,6 +10,7 @@ d = pq(url=site)
 
 #if file doesn't exist, create it. Otherwise, open it for both read/write
 f = open('site', 'a+')
+
 #get curser to the beginning, so it could be read from start to end.
 f.seek(0)
 text = f.read()
@@ -16,29 +18,20 @@ text = f.read()
 #no further use for the file for now
 f.close()
 
-print(text)
-
 #search the entire body of the HTML for the keyword
 found = d('body:contains('+search_word+')').text()
-print(found)
 
 if (found != text):
-    #print("WILL NOW WRITE CHANGES INTO site")
-
-	#if found new content in HTML, overwrite the existing text file with a new one containing all of the new informtaion
+#if found new content in HTML, overwrite the existing text file with a new one containing all of the new informtaion
 	with open('site','w') as f:
-    	f.write(found)
-	
-	# Email.
-	
+		f.write(found)
+# Email.
 	gmail_user = 'WebsiteChangeDetector.R@gmail.com'  
 	gmail_password = '123passwor'
-
 	sent_from = gmail_user  
-	to = [mail_to]  
+	to = mail_to
 	subject = 'New changes found in ' + site  
 	body = found
-
 	email_text = """\  
 	From: %s  
 	To: %s  
@@ -46,13 +39,11 @@ if (found != text):
 
 	%s
 	""" % (sent_from, ", ".join(to), subject, body)
-
 	try:  
 		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 		server.ehlo()
 		server.login(gmail_user, gmail_password)
 		server.sendmail(sent_from, to, email_text)
 		server.close()
-
-	except:  
-		print 'Something went wrong...'
+	except:
+    	print("Unexpected error")
