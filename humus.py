51b5749175f9ced22cc06a7017af2c3758ab99dd
Raz1990 <raz2712@gmail.com>
2017-10-05 08:54:48 +0300

    fixed writing and reading from file

--- humus.py
+++ humus.py
@@ -3,19 +3,23 @@ import urllib
 
 d = pq(url='https://store.pass.dmm.com/entry/tickets')
 
+#if file doesn't exist, create it. Otherwise, open it for both read/write
 f = open('site', 'a+')
+#get curser to the beginning, so it could be read from start to end.
 f.seek(0)
-
 text = f.read()
+
+#no further use for the file for now
+f.close()
+
 print(text)
 
-found = d('div:contains("Suchmos")').html()
+#search the entire body of the HTML for the keyword
+found = d('body:contains('+sys.argv[2]+')').text()
 print(found)
 
 if (found != text):
-    print("WILL NOW WRITE CHANGES INTO site")
-    f.write(found)
-else:
-    print("NO NEW CHANGES")
-
-f.close()
+    #print("WILL NOW WRITE CHANGES INTO site")
+	#if found new content in HTML, overwrite the existing text file with a new one containing all of the new informtaion
+	with open('site','w') as f:
+    	f.write(found)
